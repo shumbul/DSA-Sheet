5dc18b5f48936a62317a97d82e8256611822ae84
Manasvee Rathie <133605330+Manasvee16@users.noreply.github.com>
2025-05-27 18:32:34 +0530

    Update 45. Set Matrix Zeroes.cpp

--- Day 14/45. Set Matrix Zeroes.cpp
+++ Day 14/45. Set Matrix Zeroes.cpp
@@ -1,4 +1,3 @@
-
 class Solution 
 {
 public:
@@ -33,3 +32,55 @@ public:
 };
 //TC O(nm)
 //SC O(n+m)
+
+class Solution 
+{
+public:
+    void setZeroes(vector<vector<int>>& matrix) 
+    {
+       bool cols=1; //to track if 1st col needs to be zeroed
+	    int m=matrix.size(); //no. of rows
+        int n=matrix[0].size(); //no. of cols
+	    for(int i=0;i<m;i++)
+	    {
+		    for(int j=0;j<n;j++)
+		    {
+			    if(matrix[i][j]==0)
+			    {
+				    matrix[i][0]=0; //mark row i using 1st col
+				    if (j!=0)
+					    matrix[0][j]=0; //mark col i using 1st row
+				    else
+					    cols=0; //for 1st col
+			    }
+		    }
+	    }
+	    for(int i=1;i<m;i++)
+	    {
+		    for(int j=1;j<n;j++)
+		    {
+			    if(matrix[i][j]!=0)
+			    {
+                    if (matrix[i][0] == 0 || matrix[0][j] == 0) 
+				        matrix[i][j] = 0; //zero the cell
+                }
+            }
+        }
+	    if(matrix[0][0]==0) //1st row needs to be zeroed
+	    {
+		    for (int i = 0; i < n; i++) 
+		    {
+			    matrix[0][i] = 0;
+		    }
+        }
+	    if(cols==0) //1st col needs to be zeroed
+	    {
+		    for (int j = 0; j < m; j++) 
+		    {
+			    matrix[j][0] = 0;
+		    }
+        }
+    }
+};
+//TC O(M*N)
+//SC O(1)
