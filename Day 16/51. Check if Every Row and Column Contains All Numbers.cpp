c1e5609153801987c0de14b513f9d339c6e60653
Manasvee Rathie <133605330+Manasvee16@users.noreply.github.com>
2025-05-30 21:01:06 +0530

    Update 51. Check if Every Row and Column Contains All Numbers.cpp

--- Day 16/51. Check if Every Row and Column Contains All Numbers.cpp
+++ Day 16/51. Check if Every Row and Column Contains All Numbers.cpp
@@ -37,3 +37,39 @@ public:
 };
 //TC O(n^2)
 //SC O(n)
+
+class Solution 
+{
+public:
+    bool checkValid(vector<vector<int>>& matrix) 
+    {
+        int n=matrix.size();
+        int sumrow=0; //track row sum
+        int sumcol=0; //track column sum
+        int sum=0; //expected sum of 1 to n
+        
+        for (int i=1;i<=n;i++)
+        {
+            sum+=i;
+        }
+        for (int i=0;i<n;i++)
+        {
+            set<int> freq; //track unique elements in row i
+            for (int j=0;j<n;j++)
+            {
+                sumrow+=matrix[i][j];
+                sumcol+=matrix[j][i];
+                freq.insert(matrix[i][j]); //check row uniqueness
+            }
+            
+            if (sumrow!=sum || freq.size()!=n || sumcol!=sum)
+                return false;
+            sumrow=0;
+            sumcol=0;
+        }
+        return true;
+    }
+};
+//TC O(n^2)
+//SC O(n)
+
