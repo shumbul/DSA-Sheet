4e3d7e193eb2a45170858165266b1f3a49634b62
Manasvee Rathie <133605330+Manasvee16@users.noreply.github.com>
2025-06-01 12:38:23 +0530

    Update 61. Next Greater Element i.cpp

--- Day 19/61. Next Greater Element i.cpp
+++ Day 19/61. Next Greater Element i.cpp
@@ -31,3 +31,41 @@ public:
 };
 //TC O(n^2)
 //SC O(n)
+
+class Solution 
+{
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) 
+    {
+        if (nums1.size()>nums2.size())
+            return {};
+        vector<int> ans;
+        int n=nums1.size();
+        int m=nums2.size();
+        for (int i=0;i<n;i++)
+        {
+            for (int j=0;j<m;j++)
+            {
+                if (nums1[i]==nums2[j])
+                {
+                    int k=j+1; //look to the right
+                    while (k<m)
+                    {
+                        if (nums2[k]>nums2[j])
+                        {
+                            ans.push_back(nums2[k]);
+                            break;
+                        }
+                        k++;
+                    }
+                }
+            }
+            if (ans.size()!=i+1)
+                ans.push_back(-1);
+        }
+        return ans;
+    }
+};
+//TC O(n^3)
+//SC O(n)
+
