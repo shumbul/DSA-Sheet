5aa065f3c324464a40014d13350152ba0ae317e2
Manasvee Rathie <133605330+Manasvee16@users.noreply.github.com>
2025-06-08 19:10:54 +0530

    Create 74. Jump Game ii.cpp

---
+++ Day 24/74. Jump Game ii.cpp
@@ -0,0 +1,25 @@
+class Solution 
+{
+    vector<int> dp;
+public:
+    int jump(vector<int>& nums)
+    {
+        dp.resize(nums.size(), -1);
+        return helper(nums, 0); //start from idx 0
+    }
+    long long helper(vector<int>& nums, int idx)
+    {
+        if (idx>=nums.size()-1)
+            return 0;
+         if (dp[idx]!=-1)
+            return dp[idx];
+        long long temp=INT_MAX; //min no of jumps from this idx
+        for (int jump=0;jump<nums[idx];jump++)
+        {
+            temp=min(temp, 1+helper(nums, jump+1+idx)); //try all possible jumps from 1 to nums[idx]
+        }
+        return dp[idx]=temp;
+    }
+};
+//TC O(N*K)
+//SC O(N)
