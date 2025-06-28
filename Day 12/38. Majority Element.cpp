7136424822ded0a28d017f43d8955cff1d5a914f
Manasvee Rathie <133605330+Manasvee16@users.noreply.github.com>
2025-06-28 08:18:25 +0530

    Update 38. Majority Element.cpp

--- Day 12/38. Majority Element.cpp
+++ Day 12/38. Majority Element.cpp
@@ -31,3 +31,39 @@ public:
 
 //TC O(nlogn)
 //SC O(1)
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count=0;
+        int idx=-1;
+        int count1=0;
+        for (int i=0;i<nums.size();i++)
+        { //which no. is possible
+            if (count==0)
+            {
+                idx=nums[i];
+                count=1;
+            }
+            else if (idx==nums[i])
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+        }
+        for (int i=0;i<nums.size();i++)
+        { //confirm if that no. is possible
+            if (idx==nums[i])
+            {
+                count1++;
+            }
+        }
+        if (count1>nums.size()/2)
+            return idx;
+        return -1;
+    }
+};
+//TC O(2N)
