c7d62b2367bf173d6df56deac7228425b6670010
Manasvee Rathie <133605330+Manasvee16@users.noreply.github.com>
2025-06-30 08:49:08 +0530

    Update 1. Two Sum.cpp

--- Day 01/1. Two Sum.cpp
+++ Day 01/1. Two Sum.cpp
@@ -26,12 +26,6 @@ public:
     }
 };
 
-//map- add values less than target and its frequency
-//map- 2,7
-    //1,1
-
-
-
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -79,3 +73,34 @@ public:
         // TC: O(n), SC: O(n)
     }
 };
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<pair<int, int>> arr;
+        for (int i = 0; i < n; ++i)
+            arr.push_back({nums[i], i});
+        //Sort the array by value
+        sort(arr.begin(), arr.end());
+        int left = 0, right = n - 1;
+        while (left < right) {
+            int sum = arr[left].first + arr[right].first;
+            if (sum == target) {
+                // Return the original indices
+                return {arr[left].second, arr[right].second};
+            } 
+            else if (sum < target) 
+            {
+                left++;
+            } 
+            else 
+            {
+                right--;
+            }
+        }
+        return {}; // Not found
+    }
+};
+//TC O(N)
+//SC O(1)
