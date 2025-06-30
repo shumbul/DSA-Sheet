9ff302d83d0edc43e54591fb93ceb367104d42e1
Manasvee Rathie <133605330+Manasvee16@users.noreply.github.com>
2025-06-30 14:36:33 +0530

    Update 105. Remove Nth Node From End of List.cpp

--- Day 36/105. Remove Nth Node From End of List.cpp
+++ Day 36/105. Remove Nth Node From End of List.cpp
@@ -7,7 +7,6 @@ public:
         if (dist==n) //if the node to delete is the head
             return head->next; //skip the head and return the new head
         return dummy->next;
-
     }
     int remove(ListNode* head, int n)
     {
@@ -19,3 +18,34 @@ public:
         return distance+1; //distance from current node to end
     }
 };
+//TC O(N)
+//SC O(N)
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        // Move fast pointer n steps ahead
+        for (int i = 0; i < n; ++i) {
+            fast = fast->next;
+        }
+        // If fast is nullptr, that means we need to delete the head node
+        if (fast == nullptr) {
+            ListNode* newHead = head->next;
+            delete head; // free the memory of the old head
+            return newHead;
+        }
+        // Move both pointers until fast reaches the last node
+        while (fast->next != nullptr) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        // slow is now at the node before the one to be deleted
+        ListNode* nodeToDelete = slow->next;
+        slow->next = slow->next->next;
+        delete nodeToDelete; // free memory
+        return head; // original head is unchanged
+    }
+};
+//TC O(N)
