254f63af89e2cb20de677a9535af33f446573f85
Manasvee Rathie <133605330+Manasvee16@users.noreply.github.com>
2025-07-14 00:44:25 +0530

    Update 74. Jump Game ii.cpp

--- Day 24/74. Jump Game ii.cpp
+++ Day 24/74. Jump Game ii.cpp
@@ -1,3 +1,23 @@
+//greedy
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        int jumps = 0;
+        int farthest = 0;
+        int end = 0;
+        for (int i = 0; i < n - 1; i++) {
+            farthest = max(farthest, i + nums[i]);
+            if (i == end) {
+                jumps++;
+                end = farthest;
+            }
+        }
+        return jumps;
+    }
+};
+
+//recursion
 class Solution 
 {
     vector<int> dp;
