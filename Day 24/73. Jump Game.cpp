3150408d31ca7116be30f15d97ca8000eca737e3
Manasvee Rathie <133605330+Manasvee16@users.noreply.github.com>
2025-07-14 00:41:45 +0530

    Update 73. Jump Game.cpp

--- Day 24/73. Jump Game.cpp
+++ Day 24/73. Jump Game.cpp
@@ -1,3 +1,24 @@
+//greedy 
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxReach = 0; // Farthest index we can currently reach
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > maxReach) {
+                // If we're stuck before this index, return false
+                return false;
+            }
+            maxReach = max(maxReach, i + nums[i]);
+            if (maxReach >= nums.size() - 1) {
+                return true;
+            }
+        }
+        return true;
+    }
+};
+
+//recursion
 class Solution {
     bool ans=false;
     vector<int> dp;
