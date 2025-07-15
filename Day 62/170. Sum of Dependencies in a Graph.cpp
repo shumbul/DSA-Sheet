2c9184aabbf073cf8e50cba8a0a1f4cd593df78a
Manasvee Rathie <133605330+Manasvee16@users.noreply.github.com>
2025-07-15 12:07:45 +0530

    Create 170. Sum of Dependencies in a Graph.cpp

---
+++ Day 62/170. Sum of Dependencies in a Graph.cpp
@@ -0,0 +1,8 @@
+class Solution {
+  public:
+    // Function to calculate the sum of dependencies.
+    int sumOfDependencies(int V, vector<vector<int>> &edges) {
+        // code here
+        return edges.size();
+    }
+};
