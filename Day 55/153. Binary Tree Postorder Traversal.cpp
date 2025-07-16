0c02bf880deb8599e83f71d15d1a858cf643593f
Manasvee Rathie <133605330+Manasvee16@users.noreply.github.com>
2025-07-16 11:49:23 +0530

    Update 153. Binary Tree Postorder Traversal.cpp

--- Day 55/153. Binary Tree Postorder Traversal.cpp
+++ Day 55/153. Binary Tree Postorder Traversal.cpp
@@ -10,3 +10,28 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        if (!root) 
+            return {};
+        stack<TreeNode*> s1, s2;
+        vector<int> result;
+        s1.push(root);
+        while (!s1.empty()) {
+            TreeNode* curr = s1.top();
+            s1.pop();
+            s2.push(curr);
+            if (curr->left) 
+                s1.push(curr->left);
+            if (curr->right) 
+                s1.push(curr->right);
+        }
+        while (!s2.empty()) {
+            result.push_back(s2.top()->val);
+            s2.pop();
+        }
+        return result;
+    }
+};
