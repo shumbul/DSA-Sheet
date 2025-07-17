a405e3a271c5d951e7dc4458a2ff2d8ec47e66cb
Manasvee Rathie <133605330+Manasvee16@users.noreply.github.com>
2025-07-17 17:51:03 +0530

    Update 141. Binary Tree Inorder Traversal.cpp

--- Day 49/141. Binary Tree Inorder Traversal.cpp
+++ Day 49/141. Binary Tree Inorder Traversal.cpp
@@ -38,3 +38,52 @@ public:
 };
 //TC O(N)
 //SC O(H+N)
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        TreeNode* curr = root; //used to traverse the tree without recursion
+        while (curr != nullptr) {
+            if (curr->left == nullptr) {
+                // No left subtree, visit current node and go right
+                ans.push_back(curr->val);
+                curr = curr->right;
+            } 
+            else {
+          /* we need a way to come back to curr after finishing its left subtree.
+          To do that We find the inorder predecessor (prev) of curr.
+          We make a temporary connection: prev->right = curr
+          This acts like a thread so we can return to curr after we’ve visited everything on the left. */
+                TreeNode* prev = curr->left;
+                while (prev->right != nullptr && prev->right != curr) {
+               /*Keep moving to the rightmost node of the left subtree — but stop if you either reach the end (nullptr) or you find a thread already pointing back to the current node (curr).
+               2nd condition is to stop the loop if we have already created the thread before
+               Prevents an infinite loop.
+               Identifies that we’ve visited this left subtree already.
+               Acts like a marker to know when to stop going right */
+                    prev = prev->right;
+                }  
+                if (prev->right == nullptr) {
+                    // Create thread to curr node
+                    prev->right = curr;
+                    curr = curr->left;
+                } 
+                else {
+                    // Thread already exists, remove it and visit curr node
+                    prev->right = nullptr;
+                    ans.push_back(curr->val);
+                    curr = curr->right;
+                }
+            }
+             /*Why the else case? We already visited it?
+             No — we hadn’t visited that node yet, because we went to its left subtree first.
+             We created a thread from to remember to come back.
+             So when the node finishes, we follow the thread, and now we're back
+             Now We break the thread, We visit root node, Move to curr 
+             ✅ The point is We revisit curr after finishing its left subtree, using the thread we created in its prev.
+             This replaces the role of the call stack or explicit stack in recursive/iterative inorder traversal.*/
+        }
+        return ans;
+    }
+};
