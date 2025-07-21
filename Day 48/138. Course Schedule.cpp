36d01253b42663d90042ceef4d7614141f4cd321
Manasvee Rathie <133605330+Manasvee16@users.noreply.github.com>
2025-07-22 00:06:15 +0530

    Update 138. Course Schedule.cpp

--- Day 48/138. Course Schedule.cpp
+++ Day 48/138. Course Schedule.cpp
@@ -1,3 +1,4 @@
+//DFS
 class Solution {
     set<int> visited_dp;
     set<int> currVisited;
@@ -5,7 +6,6 @@ class Solution {
 
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-
         // 1. form a graph
         for (int i = 0; i < prerequisites.size(); i++) {
             graph[prerequisites[i][0]].push_back(prerequisites[i][1]);
@@ -34,6 +34,42 @@ public:
         //5. erase it from currVisited as we will be checking all nodes one by one in main function
         currVisited.erase(node);
         visited_dp.insert(node); //marking node as safe
+        return false;
+    }
+};
+
+//BFS
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int, vector<int>> graph;
+        vector<int> indegree(numCourses, 0);
+        int ans=0;
+        queue<int> courses;
+        for (auto i:prerequisites)
+        {
+            graph[i[1]].push_back(i[0]);
+            indegree[i[0]]++;
+        }
+        for (int i=0;i<indegree.size();i++)
+        {
+            if (indegree[i]==0)
+                courses.push(i);
+        }
+        while (!courses.empty())
+        {
+            int frontt=courses.front();
+            courses.pop();
+            ans++; //one more course successfully taken
+            for (int nbr:graph[frontt])
+            {
+                indegree[nbr]--;
+                if (indegree[nbr]==0)
+                    courses.push(nbr);
+            }
+        }
+        if (ans==numCourses) //If we successfully processed all numCourses, there is no cycle, and all courses can be completed
+            return true;
         return false;
     }
 };
