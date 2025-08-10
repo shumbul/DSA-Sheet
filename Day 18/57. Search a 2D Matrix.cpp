c08063aa693f75face4d5ed0a0fb161c504a5ba3
Manasvee Rathie <133605330+Manasvee16@users.noreply.github.com>
2025-08-11 00:21:25 +0530

    Update 57. Search a 2D Matrix.cpp

--- Day 18/57. Search a 2D Matrix.cpp
+++ Day 18/57. Search a 2D Matrix.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if (matrix.empty() || matrix[0].empty()) 
+            return false;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int row = 0;
+        int col = n - 1;  // Start at top-right
+        while (row < m && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } 
+            else if (matrix[row][col] > target) {
+                col--; // move left
+            } 
+            else {
+                row++; // move down
+            }
+        }
+        return false;
+    }
+};
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int row=0; //to track the row where the target might be found
         int m=matrix[0].size();
         int n=matrix.size();
