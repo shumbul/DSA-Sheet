b7656fa1acb5e03d53c59fc50a9a0c33e4f9bbaf
Manasvee Rathie <133605330+Manasvee16@users.noreply.github.com>
2025-08-14 02:12:23 +0530

    Update 130. Clone Graph.cpp

--- Day 45/130. Clone Graph.cpp
+++ Day 45/130. Clone Graph.cpp
@@ -20,3 +20,48 @@ public:
 };
 //TC O(V+E)
 //SC O(E)
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        // If the given graph is empty, return null
+        if (!node)
+            return nullptr;
+        // Queue for BFS traversal of the graph
+        queue<Node*> nodes;
+        nodes.push(node);
+        // Map to store the mapping between original node and its cloned node
+        // Key: pointer to original node
+        // Value: pointer to cloned node
+        unordered_map<Node*, Node*> orders;
+        // Create the first cloned node (copy of the starting node)
+        Node* clonenode = new Node(node->val);
+        // Store mapping from original starting node to cloned node
+        orders[node] = clonenode;
+        // BFS traversal
+        while (!nodes.empty()) {
+            int sizee = nodes.size(); // Number of nodes at this BFS level
+            while (sizee--) { // Process all nodes in current level
+                auto frontt = nodes.front(); // Get the first node from queue
+                nodes.pop();
+                // Iterate through all neighbors of the current node
+                for (Node* i : frontt->neighbors) {
+                    // If this neighbor hasn't been cloned yet
+                    if (orders.find(i) == orders.end()) {
+                        // Push the original neighbor into queue for BFS
+                        nodes.push(i);
+                        // Create a new cloned node for this neighbor
+                        Node* child = new Node();
+                        child->val = i->val;
+                        // Store mapping from original neighbor to cloned neighbor
+                        orders[i] = child;
+                    }
+                    // Link the cloned neighbor to the cloned current node's neighbors list
+                    orders[frontt]->neighbors.push_back(orders[i]);
+                }
+            }
+        }
+        // Return the cloned starting node (entire cloned graph is connected from here)
+        return orders[node];
+    }
+};
